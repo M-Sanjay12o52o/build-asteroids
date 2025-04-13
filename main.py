844354fb42e5348b6fd232a66ca7cf2fe6a97831
M-Sanjay12o52o <msnjy12o52o@gmail.com>
2025-04-13 14:37:22 +0530

    done

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
 import sys
+from shot import Shot
 
 
 def main():
@@ -14,54 +15,59 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
     AsteroidField.containers = updatable
     asteroidfield = AsteroidField()
 
     Player.containers = (updatable, drawable)
 
-    x = SCREEN_WIDTH / 2
-    y = SCREEN_HEIGHT / 2
-    player = Player(x, y)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
+                return
 
         # player.update(dt)
         updatable.update(dt)
 
+        # asteroid descrution
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
+
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    shot.kill()
+                    asteroid.split()
+
         # Fill the screen with black background
-        black = (0, 0, 0)
-        screen.fill(black)
+        screen.fill("black")
 
         # Draw the player
         # player.draw(screen)
         # drawable.draw(screen)
-        for sprite in drawable:
-            sprite.draw(screen)
+        # for sprite in drawable:
+        # sprite.draw(screen)
 
         # Draw shots separately if you want additional styling
-        player.shots_group.update(dt)
-        player.shots_group.draw(screen)
+        # player.shots_group.update(dt)
+        # player.shots_group.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
 
         # Update the display
         pygame.display.flip()
 
         # Calculate the delta time for smoother movement (if needed)
         dt = clock.tick(60) / 1000.0
-        print(f"Delta time: {dt:.3f} seconds")
-
-        # asteroid descrution
-        for asteroid in asteroids:
-            for bullet in player.shots_group:
-                if asteroid.rect.colliderect(bullet.rect):
-                    asteroid.kill()
 
 
 if __name__ == "__main__":
